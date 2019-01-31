584b64882f77fa758d67cda28fd1d74cb93aa555
olewaHHH <517259854@qq.com>
2019-01-31 22:54:10 +0800

    deploy 1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: '/blog-demo/',
+  title: 'blog-demo',
+  description: 'Vuepress blog demo',
+  themeConfig: {
+  	// 你的GitHub仓库
+    repo: 'https://github.com/olewaHHH/blog-demo',
+    // 自定义仓库链接文字。
+    repoLabel: 'My GitHub',
+  	nav: [
+  		{ text: 'Home', link: '/' },
+  		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+  	],
+  	sidebar: [
+      ['/', '首页'],
+      ['/blog/FirstBlog.md', '我的第一篇博客']
+    ]
+  }
+}
